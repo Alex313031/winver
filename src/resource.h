3a63e6ed0a997b003440e8e8e6df22078d8f32b4
Alexander Frick <Alex313031@gmail.com>
2025-12-05 19:51:44 -0600

    corrections and clang-format for source

--- src/resource.h
+++ src/resource.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #define IDD_WINVER               100
+
 #define IDC_WINVER               101
 #define IDC_WINVER_COPYRIGHT     102
 
-#define IDS_WIN95                 20
-#define IDS_WIN98                 21
-#define IDS_COPYRIGHT_WIN95       22
-#define IDS_COPYRIGHT_WIN98       23
-#define IDS_COPYRIGHT_WIN98SE     24
-#define IDS_WINME                 25
-#define IDS_COPYRIGHT_WINME       26
+#define IDI_WINVER               106
+
+#define IDS_WIN95                20
+#define IDS_WIN98                21
+#define IDS_COPYRIGHT_WIN95      22
+#define IDS_COPYRIGHT_WIN98      23
+#define IDS_COPYRIGHT_WIN98SE    24
+#define IDS_WINME                25
+#define IDS_COPYRIGHT_WINME      26
+
+#ifndef IDC_STATIC
+ #define IDC_STATIC              -1
+#endif // IDC_STATIC
--- src/winver9x.c
+++ src/winver9x.c
@@ -4,8 +4,9 @@
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
 
-enum WINVER9XMODE
-{
+static HANDLE gHinst;
+
+enum WINVER9XMODE {
 	WV9XM_WIN95 = 0,
 	WV9XM_WIN98,
 	WV9XM_WIN98SE,
@@ -18,44 +19,48 @@ const BYTE c_rgWinverIDs[WV9XM_COUNT]
 const BYTE c_rgWinverCopyrightIDs[WV9XM_COUNT]
 	= { IDS_COPYRIGHT_WIN95, IDS_COPYRIGHT_WIN98, IDS_COPYRIGHT_WIN98SE, IDS_COPYRIGHT_WINME };
 
-INT_PTR CALLBACK WinverDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
+INT_PTR CALLBACK WinverDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	HKEY hKey;
 	DWORD dwMode = 0;
 	DWORD cbMode = sizeof(DWORD);
 	TCHAR szBuffer[64];
 
-	switch (uMsg)
-	{
+	switch (uMsg) {
 		case WM_INITDIALOG:
 			RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("SOFTWARE\\winver9x"), NULL, KEY_READ, &hKey);
-			if (hKey)
-			{
+			if (hKey) {
 				RegQueryValueEx(hKey, TEXT("Mode"), NULL, NULL, &dwMode, &cbMode);
 				RegCloseKey(hKey);
 			}
 
-			if (dwMode >= WV9XM_COUNT)
+			if (dwMode >= WV9XM_COUNT) {
 				dwMode = WV9XM_WIN95;
+      }
 
-			LoadString(HINST_THISCOMPONENT, c_rgWinverIDs[dwMode], szBuffer, ARRAYSIZE(szBuffer));
+			LoadString(gHinst, c_rgWinverIDs[dwMode], szBuffer, ARRAYSIZE(szBuffer));
 			SetDlgItemText(hwnd, IDC_WINVER, szBuffer);
 
-			LoadString(HINST_THISCOMPONENT, c_rgWinverCopyrightIDs[dwMode], szBuffer, ARRAYSIZE(szBuffer));
+			LoadString(gHinst, c_rgWinverCopyrightIDs[dwMode], szBuffer, ARRAYSIZE(szBuffer));
 			SetDlgItemText(hwnd, IDC_WINVER_COPYRIGHT, szBuffer);
 
-			return TRUE;
+			return (INT_PTR)TRUE;
 		case WM_COMMAND:
 			EndDialog(hwnd, 0);
-			return TRUE;
+			return (INT_PTR)TRUE;
 		default:
-			return FALSE;
+			return (INT_PTR)FALSE;
 	}
 }
 
-int WINAPI WinMainCRTStartup(void)
-{
-	DialogBoxParam(HINST_THISCOMPONENT, MAKEINTRESOURCE(IDD_WINVER), NULL, WinverDlgProc, NULL);
+// The main entry point, equivalent to int main()
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow) {
+  // Contains HINSTANCE from previous running instance, unused.
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  gHinst = hInstance;
+	DialogBoxParam(gHinst, MAKEINTRESOURCE(IDD_WINVER), NULL, WinverDlgProc, NULL);
 	ExitProcess(0);
 	return 0;
 }
